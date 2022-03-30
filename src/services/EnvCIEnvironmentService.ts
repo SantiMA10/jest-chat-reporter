67f87bdb24dc72225397cc4b39ffcd7d057dee6a
Santiago Martín Agra <santiagomartinagra@gmail.com>
2022-03-30 19:54:57 +0200

    feat: return repository slug if the build url is not available

--- src/services/EnvCIEnvironmentService.ts
+++ src/services/EnvCIEnvironmentService.ts
@@ -3,18 +3,28 @@ import envCi from 'env-ci';
 import { EnvironmentService } from './EnvironmentService';
 
 export class EnvCIEnvironmentService implements EnvironmentService {
-	public isCI: EnvironmentService['isCI'] = () => envCi().isCi;
+	public constructor(private readonly env: typeof envCi = envCi) {}
+
+	public isCI: EnvironmentService['isCI'] = () => this.env().isCi;
 
 	public getServiceName: EnvironmentService['getServiceName'] = () => {
-		const ci = envCi();
+		const ci = this.env();
 		return ci.isCi ? ci.service : 'local';
 	};
 
 	public getBuildUrl: EnvironmentService['getBuildUrl'] = () => {
-		const ci = envCi();
+		const ci = this.env();
 
-		if ('buildUrl' in ci) {
+		if (!ci.isCi) {
+			return 'localhost';
+		}
+
+		if ('buildUrl' in ci && !!ci.buildUrl) {
 			return ci.buildUrl;
+		}
+
+		if ('slug' in ci) {
+			return ci.slug;
 		}
 
 		return 'localhost';
